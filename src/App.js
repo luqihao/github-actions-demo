6a14600548e74220dc0f238e38c3f1d3992fb9d4
luqihao <1021008256@qq.com>
2020-01-02 16:39:57 +0800

    chore: change title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          gh-pages
+          gh-pages-cnsusu
         </a>
       </header>
     </div>
